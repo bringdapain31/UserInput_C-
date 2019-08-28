ce75a9b354a90522f4ba7e5d40c8225d176e39be
Preston LaMarca <bane3154@gmail.com>
2019-08-28 11:47:06 -0700

    Add files via upload

---
+++ UserInput/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace UserInput
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Basic program getting Users First and Last name and age and then printing Hello users name and age */
+
+            /*Console.Write("Enter your First and Last name: ");
+            string name = Console.ReadLine();
+            Console.Write("Enter your age: ");
+            string age = Console.ReadLine();
+            Console.WriteLine("Hello " + name + " you are " + age + " years old ");
+
+
+            Console.ReadLine(); */
+
+            /* Building A Basic Calculator */
+
+            /* Converting a string into a number, without Convert. C# would YELL at us with an error */
+            /* int num = Convert.ToInt32("45");
+            Console.WriteLine(num + 6); */
+
+            /* Console.Write("Enter a number: ");
+             double num1 = Convert.ToDouble(Console.ReadLine());
+             Console.Write("Enter another number: ");
+             double num2 = Convert.ToDouble(Console.ReadLine());
+
+             Console.WriteLine(num1 + num2);
+
+
+             Console.ReadLine(); */
+
+
+        }
+    }
+}
